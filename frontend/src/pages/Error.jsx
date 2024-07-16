c9d37b724f33b9cb244235d92adb7ca8790ab465
allysanfoot <allysafoot@gmail.com>
2024-07-16 11:58:51 -0700

    feat: 404 Not Found component/page

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Error() {
     return (
-        <div>Error</div>
+        <div>
+            <h1>404 Not Found</h1>
+            <p>Sorry, the page you are looking for does not exist.</p>
+        </div>
     )
 }
 
