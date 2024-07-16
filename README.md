4ad3783d722b95bcbc64012777d1cf5fcdc92194
allysanfoot <allysafoot@gmail.com>
2024-07-16 11:57:51 -0700

    feat: setting up routes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # let-em-cook
+
+To begin virtual environment within the current PowerShell session, you will need to run the following command from the home directory: .\env\Scripts\activate.ps1.
+
+If you encounter an error stating, "..\env\Scripts\Activate.ps1 cannot be loaded because running scripts is disabled on the system" due to Execution Policy Settings, you will need to execute the following command first: Set-ExecutionPolicy Unrestricted -Scope Process. Then run the command above, which should launch the virtual environment.
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,33 @@
 import React from "react"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Homepage from "./pages/Homepage"
+import Error from "./pages/Error"
+import ProtectedRoutes from "./components/ProtectedRoutes"
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterThenLogout() {
+  // so we don't have any old access tokens lying around
+  localStorage.clear()
+  return <Register />
+}
 
 function App() {
 
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProtectedRoutes><Homepage /></ProtectedRoutes>} />
+        <Route path="/register" element={<RegisterThenLogout />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   )
 }
 
