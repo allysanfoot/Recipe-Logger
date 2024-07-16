2307ae83e1ea4dce1383f0e2aed6e66e99051481
allysanfoot <allysafoot@gmail.com>
2024-07-16 16:30:30 -0700

    fix: remove image field from recipe model

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 class Recipe(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField(upload_to='images/')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -14,36 +14,68 @@ function Homepage() {
     const createRecipe = (e) => {
         e.preventDefault()
         api
-        .post('api/recipes/', {title: title, content: content})
-        .then((response) => {
-            if (response.status === 201) {alert('Recipe created')}
-            else alert('Failed to create recipe')
-            getRecipes()
-        })
-        .catch((error) => alert(error))
+            .post('api/recipes/', { title: title, content: content })
+            .then((response) => {
+                if (response.status === 201) { alert('Recipe created') }
+                else alert('Failed to create recipe')
+                getRecipes()
+            })
+            .catch((error) => alert(error))
     }
 
     const getRecipes = () => {
         api
-        .get('/api/recipes/')
-        .then((response) => response.data)
-        .then((data) => {setRecipes(data); console.log(data)})
-        .catch((error) => alert(error))
+            .get('/api/recipes/')
+            .then((response) => response.data)
+            .then((data) => { setRecipes(data); console.log(data) })
+            .catch((error) => alert(error))
     }
 
     const deleteRecipe = (id) => {
         api
-        .delete(`/api/recipes/delete/${id}/`)
-        .then((response) => {
-            if (response.status === 204) {alert('Recipe deleted')}
-            else alert('Failed to delete recipe')
-        })
-        .catch((error) => alert(error))
+            .delete(`/api/recipes/delete/${id}/`)
+            .then((response) => {
+                if (response.status === 204) { alert('Recipe deleted') }
+                else alert('Failed to delete recipe')
+            })
+            .catch((error) => alert(error))
         getRecipes()
     }
 
     return (
-        <div>Homepage</div>
+        <>
+            <div>
+                <h2>Recipes</h2>
+            </div>
+            <div>
+                <h2>Create a Recipe!</h2>
+                <form onSubmit={createRecipe}>
+                    <label htmlFor='title'>Title:</label>
+                    <br />
+                    <input
+                        type='text'
+                        id='title'
+                        name='title'
+                        required
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                    />
+                    <br />
+                    <br />
+                    <label htmlFor='content'>Content:</label>
+                    <br />
+                    <textarea
+                        id='content'
+                        name='content'
+                        required
+                        onChange={(e) => setContent(e.target.value)}
+                        value={content}>
+                    </textarea>
+                    <br />
+                    <input type='submit' value='Create Recipe' />
+                </form>
+            </div>
+        </>
     )
 }
 
