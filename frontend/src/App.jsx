6ccd9f2fb1e27b54f8fe15cd04887a0cf12b069a
allysanfoot <allysafoot@gmail.com>
2024-07-19 11:03:37 -0700

    fix: removed route form app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,6 @@ function App() {
         <Route path="/register" element={<RegisterThenLogout />} />
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
-        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   )
